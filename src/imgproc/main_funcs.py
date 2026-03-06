a897b7438c0b54a8df3fbad1564ad598f90b3e2c
Artem Novokshonov <nov.art.ig@gmail.com>
2026-03-06 09:46:39 +0100

    Update main_funcs.py

--- src/imgproc/main_funcs.py
+++ src/imgproc/main_funcs.py
@@ -2,8 +2,26 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 
+
+def read_img(path):
+    # Reading images
+    try:
+      if path[-4:] == '.txt':
+        img = np.loadtxt(path)
+      else:
+        img = np.array(PIL.Image.open(path))
+    except:
+      print('ERROR: Something went wrong while image reading!!!')
+    return img
+
+
 def img_proj(img):
   # Image projections
   hor_proj = np.sum(img,0)
   vrt_proj = np.sum(img,1)
   return hor_proj, vrt_proj
+
+
+def movmean_1d(ar, _n=5):
+    # Moving average/mean
+    return np.convolve(ar, np.ones(_n)/_n, mode='same')
